ef4437fb7f181c782afc01a6f34520a285fe505b
EMANUEL-Y700\Emanuel <emacupk@gmail.com>
2017-08-12 00:16:47 -0300

    Users can see survey results

--- client/src/index.js
+++ client/src/index.js
@@ -6,10 +6,12 @@ import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
 
 import App from "./components/App";
+import axios from 'axios';
+
 import reducers from './reducers';
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
-
+window.axios = axios;
 ReactDOM.render(
   <Provider store={store}><App /></Provider>,
   document.querySelector("#root"));
